a269fb0fb6cceff9615d0414f60ff59653a98b3a
bkrayoub <92027292+bkrayoub@users.noreply.github.com>
2024-02-08 11:56:13 +0100

    .

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let menuBtn = document.getElementById('menuBtn')
 let menuList = document.getElementById('menuList')
+let discover = document.getElementById('discover')
+let buttonHoverBg = document.getElementById('buttonHoverBg')
 let allImages = document.querySelectorAll('img')
 
 let menuOpened = false
 
+
+
+discover.addEventListener('mouseover', function(){
+    buttonHoverBg.style.width = '97%'
+})
+discover.addEventListener('mouseleave', function(){
+    buttonHoverBg.style.width = '3%'
+})
+
 menuBtn.addEventListener('click', () => {
     if (menuOpened) {
-
+        menuList.style.width = "0%"
         menuOpened = false
+    }
+    else {
+        menuList.style.width = "10%"
+        menuOpened = true
     }
 })
